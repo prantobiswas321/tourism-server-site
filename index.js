6cf8779fc2e8076a7abca6c3d56044371bf6149d
prantobiswas321 <rahulbd321@gmail.com>
2021-10-30 15:22:46 +0600

    getting data

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express');
+const { MongoClient, MongoRuntimeError } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ujyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('tourism_book');
+        const placesCollection = database.collection('places');
+
+        app.get('/places', async (req, res) => {
+            const cursor = placesCollection.find();
+            const places = await cursor.toArray();
+            res.send(places);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Tourism server is running');
